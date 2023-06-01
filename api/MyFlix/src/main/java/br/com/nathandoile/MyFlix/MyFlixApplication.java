404edd2e60483fecad9f242a0f296b58544b744a
NathanDoile <doile.souza@gmail.com>
2023-06-01 17:31:54 -0300

    Adicionando projeto base api

---
+++ api/MyFlix/src/main/java/br/com/nathandoile/MyFlix/MyFlixApplication.java
@@ -0,0 +1,13 @@
+package br.com.nathandoile.MyFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFlixApplication.class, args);
+	}
+
+}
